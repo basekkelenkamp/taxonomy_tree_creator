5cc4e3d72f1de7d23b0bd3f93d9ba39e9a149c8a
Bas Ekkelenkamp <basekkelenkamp@MacBook-Pro.fritz.box>
2022-12-30 15:32:23 +0100

    capital gain

--- capital_gain_taxonimy_tree.py
+++ capital_gain_taxonimy_tree.py
@@ -1,26 +1,29 @@
 import pandas as pd
 
-STEPS = [0, 100, 500, 1000, 2500, 7500]
+STEPS = [0, 500, 1000, 2000, 4000, 8000]
 counts = [0] * len(STEPS)
 
 
 def calculate_row_range(row_, level):
     step = STEPS[level]
     counts[level] += 1
-    if counts[level] > step:
+    if counts[level] >= step:
         counts[level] = 0
 
     return f"{row_ - counts[level]}-{row_ - counts[level] + step}"
 
 
 if __name__ == "__main__":
-    adult_df = pd.read_csv("adult-dataset.csv")
+    adult_df = pd.read_csv("input-data/adult-dataset.csv")
     cap = adult_df["capitical-gain"].unique()
 
-    rows = range(min(cap), max(cap)+1, 500)
+    rows = range(min(cap), max(cap), 500)
+    combined_rows = list(rows)
+    combined_rows.extend(cap.tolist())
+    combined_rows = sorted(set(combined_rows))
     cap_df = pd.DataFrame(columns=[f"level-{i}" for i, step in enumerate(STEPS)])
 
-    for row in rows:
+    for row in combined_rows:
         level_1 = calculate_row_range(row, 1)
         level_2 = calculate_row_range(row, 2)
         level_3 = calculate_row_range(row, 3)
@@ -42,7 +45,11 @@ if __name__ == "__main__":
         )
         cap_df = pd.concat([cap_df, df], ignore_index=True)
 
-    breakpoint()
-    cap_df.to_csv("capital_gain_taxonimy_tree.csv", index=False)
+    filename = "output-data/scenario-a-detailed-tax-capital-gain.csv"
+    print(f"Filename: {filename}")
+    print("generate tree? ('y' or 'n')")
+
+    if 'y' in input():
+        cap_df.to_csv(filename, index=False)
 
     breakpoint()
